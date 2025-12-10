6b85ea49f9d203eb6332c6904de77fc574ca8018
filbert288 <filbert.tedja6@gmail.com>
2025-12-10 08:21:56 +0700

    kita gausah pake name, phone, email

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ const form = document.querySelector(".reg-form");
 
 form.addEventListener("submit", function (e) {
     const username = document.getElementById("username").value.trim();
-    const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
-    const phone = document.getElementById("phoneNum").value.trim();
     const password = document.getElementById("password").value;
     const conPassword = document.getElementById("conPassword").value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phonePattern = /^[0-9]{8,15}$/;
 
-    if (!username || !name || !email || !phone || !password || !conPassword) {
+    if (!username || !email || !password || !conPassword) {
         alert("Semua field wajib diisi!");
         e.preventDefault();
         return;
@@ -18,12 +14,6 @@ form.addEventListener("submit", function (e) {
 
     if (!emailPattern.test(email)) {
         alert("Format email tidak valid!");
-        e.preventDefault();
-        return;
-    }
-
-    if (!phonePattern.test(phone)) {
-        alert("Nomor telepon hanya boleh angka (8–15 digit).");
         e.preventDefault();
         return;
     }
@@ -42,3 +32,4 @@ form.addEventListener("submit", function (e) {
 
     alert("Registrasi berhasil!");
 });
+
